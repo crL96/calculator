049275ad9232faf7cf7eb96c6bde4cbab8a828ea
crL96 <190506813+crL96@users.noreply.github.com>
2025-01-08 16:24:09 +0100

    Add function to numerical and AC buttons

--- javascript.js
+++ javascript.js
@@ -16,8 +16,84 @@ function divide(a, b) {
 
 let a = 0;
 let b = 0;
-let operator;
+let operator = null;
+let currentNumber = "";
 
 function operate(operator, a, b) {
     return operator(a, b);
 }
+
+const display = document.querySelector("#display");
+
+
+// All numerical buttons
+const btn1 = document.querySelector("#btn1");
+btn1.addEventListener("click", () => {
+    currentNumber = currentNumber + "1";
+    display.textContent = currentNumber;
+});
+
+const btn2 = document.querySelector("#btn2");
+btn2.addEventListener("click", () => {
+    currentNumber = currentNumber + "2";
+    display.textContent = currentNumber;
+});
+
+const btn3 = document.querySelector("#btn3");
+btn3.addEventListener("click", () => {
+    currentNumber = currentNumber + "3";
+    display.textContent = currentNumber;
+});
+
+const btn4 = document.querySelector("#btn4");
+btn4.addEventListener("click", () => {
+    currentNumber = currentNumber + "4";
+    display.textContent = currentNumber;
+});
+
+const btn5 = document.querySelector("#btn5");
+btn5.addEventListener("click", () => {
+    currentNumber = currentNumber + "5";
+    display.textContent = currentNumber;
+});
+
+const btn6 = document.querySelector("#btn6");
+btn6.addEventListener("click", () => {
+    currentNumber = currentNumber + "6";
+    display.textContent = currentNumber;
+});
+
+const btn7 = document.querySelector("#btn7");
+btn7.addEventListener("click", () => {
+    currentNumber = currentNumber + "7";
+    display.textContent = currentNumber;
+});
+
+const btn8 = document.querySelector("#btn8");
+btn8.addEventListener("click", () => {
+    currentNumber = currentNumber + "8";
+    display.textContent = currentNumber;
+});
+
+const btn9 = document.querySelector("#btn9");
+btn9.addEventListener("click", () => {
+    currentNumber = currentNumber + "9";
+    display.textContent = currentNumber;
+});
+
+const btn0 = document.querySelector("#btn0");
+btn0.addEventListener("click", () => {
+    currentNumber = currentNumber + "0";
+    display.textContent = currentNumber;
+});
+
+
+//Special buttons
+const btnAC = document.querySelector("#btnAC");
+btnAC.addEventListener("click", () => {
+    currentNumber = "";
+    a = 0;
+    b = 0;
+    operator = null;
+    display.textContent = currentNumber;
+});
