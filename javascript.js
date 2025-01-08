9b72dbffc45646ef890ef28046b767c3708b40bd
crL96 <190506813+crL96@users.noreply.github.com>
2025-01-08 17:10:53 +0100

    Add and rename div + reference

--- javascript.js
+++ javascript.js
@@ -27,68 +27,68 @@ let b = 0;
 let operator = null;
 let currentNumber = "";
 
-const display = document.querySelector("#display");
+const displayNr = document.querySelector("#displayNr");
 
 
 // All numerical buttons
 const btn1 = document.querySelector("#btn1");
 btn1.addEventListener("click", () => {
     currentNumber = currentNumber + "1";
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
 
 const btn2 = document.querySelector("#btn2");
 btn2.addEventListener("click", () => {
     currentNumber = currentNumber + "2";
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
 
 const btn3 = document.querySelector("#btn3");
 btn3.addEventListener("click", () => {
     currentNumber = currentNumber + "3";
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
 
 const btn4 = document.querySelector("#btn4");
 btn4.addEventListener("click", () => {
     currentNumber = currentNumber + "4";
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
 
 const btn5 = document.querySelector("#btn5");
 btn5.addEventListener("click", () => {
     currentNumber = currentNumber + "5";
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
 
 const btn6 = document.querySelector("#btn6");
 btn6.addEventListener("click", () => {
     currentNumber = currentNumber + "6";
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
 
 const btn7 = document.querySelector("#btn7");
 btn7.addEventListener("click", () => {
     currentNumber = currentNumber + "7";
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
 
 const btn8 = document.querySelector("#btn8");
 btn8.addEventListener("click", () => {
     currentNumber = currentNumber + "8";
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
 
 const btn9 = document.querySelector("#btn9");
 btn9.addEventListener("click", () => {
     currentNumber = currentNumber + "9";
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
 
 const btn0 = document.querySelector("#btn0");
 btn0.addEventListener("click", () => {
     currentNumber = currentNumber + "0";
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
 
 
@@ -99,7 +99,7 @@ btnAC.addEventListener("click", () => {
     a = 0;
     b = 0;
     operator = null;
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
 
 
@@ -109,7 +109,7 @@ btnAdd.addEventListener("click", () => {
     a = Number(currentNumber);
     currentNumber = "";
     operator = add;
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
 
 const btnSubtract = document.querySelector("#btnSubract");
@@ -117,7 +117,7 @@ btnSubtract.addEventListener("click", () => {
     a = Number(currentNumber);
     currentNumber = "";
     operator = subtract;
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
 
 const btnMultiply = document.querySelector("#btnMultiply");
@@ -125,7 +125,7 @@ btnMultiply.addEventListener("click", () => {
     a = Number(currentNumber);
     currentNumber = "";
     operator = multiply;
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
 
 const btnDivide = document.querySelector("#btnDivide");
@@ -133,7 +133,7 @@ btnDivide.addEventListener("click", () => {
     a = Number(currentNumber);
     currentNumber = "";
     operator = divide;
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
 
 const btnMod = document.querySelector("#btnMod");
@@ -141,12 +141,12 @@ btnMod.addEventListener("click", () => {
     a = Number(currentNumber);
     currentNumber = "";
     operator = modulus;
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
 
 const btnEquals = document.querySelector("#btnEquals");
 btnEquals.addEventListener("click", () => {
     b = Number(currentNumber);
     currentNumber = operate(operator, a, b);
-    display.textContent = currentNumber;
+    displayNr.textContent = currentNumber;
 });
